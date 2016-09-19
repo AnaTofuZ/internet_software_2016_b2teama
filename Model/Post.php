9534b15635af34d927f40ceabb30389a290c3116
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2016-09-19 14:01:35 +0900

    アソシエーション仕様のためにPostModel作成

--- Model/Post.php
+++ Model/Post.php
@@ -3,6 +3,7 @@ App::uses('AppModel', 'Model');
 /**
  * Post Model
  *
+ * @property Users $Users
  */
 class Post extends AppModel {
 
@@ -20,4 +21,21 @@ class Post extends AppModel {
  */
 	public $displayField = 'title';
 
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Users' => array(
+			'className' => 'Users',
+			'foreignKey' => 'users_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
