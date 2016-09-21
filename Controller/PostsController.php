db85b90811de76191a416100d58c67f75aa85a13
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2016-09-21 19:24:06 +0900

    軽いcomment追加。まだ錯誤中

--- Controller/PostsController.php
+++ Controller/PostsController.php
@@ -53,20 +53,30 @@ class PostsController extends AppController {
 
     public function add(){
         $user = $this->Auth->user();
+        /*
+         * 既にAuthでログイン済みなのでこの時点で$userには
+         * Array ( [id] => 1014888828 [id_hush] => 略
+         * [name] => 八雲アナグラ [screen_name] => AnaTofuZ
+         *  [access_token_key] => 1014888828-21sBDgbmfnAi6gHv8CmXaT4ruIvM7u96ZZRL6Sx
+         * [access_token_secret] => OB9sO8oO1sY0tYVk9yVtiQmBlikbkkEXRhNU8qRZjNa1n )
+         * が入っている
+         */
+        //print_r($user);
 
         if($this->request->is('post')){
          $this->Post->create();
          if($this->Post->save($this->request->data)){
-             $post = $this->Post->find('all'); //$postにPost->find出来た要素を入れる。これだとおｋ
+             //$post = $this->Post->find('all'); //$postにPost->find出来た要素を入れる。これだとおｋ
 
 
-         /*    エラーの原因はここみたい
+         //    エラーの原因はここみたい
               $post["Post"]["id"] = $this->Post["Post"]["id"];
          $post["Post"]["title"] = $this->post["Post"]["title"];
          $post["Post"]["body"] = $this->post["Post"]["body"];
          $post["Post"]["created"] = $this->post["Post"]["created"];
          $post["Post"]["modified"] = $this->post["Post"]["modified"];
-       */
+
+         $post["Post"]["user_id_hush "] = $user->id_hush;
          $this->Post->save($post);}
           $this->Session->setFlash(_('Succesed post.'),'default');
          return $this->redirect(array('action' => 'index'));
